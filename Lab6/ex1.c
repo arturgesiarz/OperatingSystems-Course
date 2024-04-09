7aac8bdab1a216c667a8e3894d2221668e9eee32
Artur Gęsiarz <arturgesiarz@interia.pl>
2024-04-09 16:53:58 +0000

    Ex1 was done

--- Lab6/ex1.c
+++ Lab6/ex1.c
@@ -1,5 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
 
+double function(double x) {
+    return 4 / (x * x + 1);
+}
 
-int main () {
+double integrate_function() {
+    return M_PI;
+}
 
+double calculate_integral(double start, double end, double width) {
+    double sum = 0;
+    double x;
+    for (x = start; x < end; x += width) {
+        sum += function(x) * width;
+    }
+    return sum;
+}
+
+int main(int argc, char *argv[]) {
+
+    double width = atof(argv[1]);
+    int num_processes = atoi(argv[2]);
+    double interval = 1.0 / num_processes;
+
+    int fd[num_processes][2];
+    double results[num_processes];
+
+    for (int i = 0; i < num_processes; i++) {
+        if (pipe(fd[i]) == -1) {
+            perror("Pipe creation failed");
+            return 1;
+        }
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("Fork failed");
+            return 1;
+
+        } else if (pid == 0) {
+            close(fd[i][0]);
+
+            double start = i * interval;
+            double end = start + interval;
+            double result = calculate_integral(start, end, width);
+
+            write(fd[i][1], &result, sizeof(double));
+            close(fd[i][1]);
+
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < num_processes; i++) {
+        close(fd[i][1]);
+        read(fd[i][0], &results[i], sizeof(double));
+        close(fd[i][0]);
+    }
+
+    double total_result = 0;
+    for (int i = 0; i < num_processes; i++) {
+        total_result += results[i];
+    }
+
+    printf("Total result: %f\n", total_result);
+    printf("Real value is: %f\n", integrate_function());
+    return 0;
 }
