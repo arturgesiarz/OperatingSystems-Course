b40fd5024f34d4004ad144a66739a17d7c526900
Artur Gęsiarz <arturgesiarz@interia.pl>
2024-06-12 08:43:33 +0000

    Client.c was implemented in lab11

--- Lab11/client.c
+++ Lab11/client.c
@@ -11,3 +11,122 @@
 #include <string.h>
 
 #include "prot_spec.h"
+
+volatile bool should_close = false;
+
+void sigint_handler(int signo) {
+    should_close = true;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Usage %s <ip> <port> <self identifier \n", argv[0]);
+        return -1;
+    }
+
+    signal(SIGINT, sigint_handler);
+
+    char* client_identifier = argv[1];
+
+    // konwersuje addres
+    uint32_t ip_address = inet_addr(argv[2]);
+
+    // pozyskiwanie portu przez konwersje na longa za pomoca strtol
+    uint16_t port = (uint16_t) strtol(argv[3], NULL, 10);
+
+    // ...
+    struct sockaddr_in addr = {
+        .sin_addr.s_addr = ip_address,
+        .sin_port = htons(port),
+        .sin_family = AF_INET
+    };
+
+    // ...
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (connect(socket_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+        perror("connect");
+
+    request_message_t alive_message = {
+            .request_type = ALIVE
+    };
+
+    // ...
+    strncpy(alive_message.sender_client_id, client_identifier, MAX_CLIENT_ID_LEN);
+    send(socket_fd, &alive_message, sizeof(alive_message), MSG_DONTWAIT);
+
+    pid_t listener_pid = fork();
+
+    // ...
+    if (listener_pid < 0)
+        perror("fork listener");
+    // ...
+    else if (listener_pid == 0) {
+        while (!should_close) {
+            request_message_t message;
+            recv(socket_fd, &message, sizeof(message), MSG_WAITALL);
+            switch (message.request_type) {
+                case LIST:
+                    for (int i = 0; i < message.payload.list.list_length; i++) {
+                        printf("%s\n",message.payload.list.identifiers_list[i]);
+                    }
+                    break;
+                case TOALL:
+                    printf("2ALL from: %s: %s\n",message.sender_client_id, message.payload.to_all);
+                    break;
+                case TOONE:
+                    printf("2ONE from: %s: %s\n",message.sender_client_id, message.payload.to_one.message);
+                    break;
+                case ALIVE:
+                    send(socket_fd, &alive_message, sizeof(alive_message), MSG_DONTWAIT);
+                    break;
+                default:
+                    printf("Invalid response type!\n");
+                    break;
+            }
+
+        }
+    }
+    // ...
+    else {
+        char* request_type_input_buffer = NULL;
+        while (!should_close) {
+            if (scanf("Input type: %ms", &request_type_input_buffer) == 1) {
+                request_message_t message;
+                strncpy(message.sender_client_id, client_identifier, MAX_CLIENT_ID_LEN);
+
+                // ...
+                if (strncmp(request_type_input_buffer, "LIST", 4) == 0) {
+                    message.request_type = LIST;
+                    send(socket_fd, &message, sizeof(message), MSG_DONTWAIT);
+                } else if (strncpy(request_type_input_buffer, "2ALL", 4) == 0) {
+                    message.request_type = TOALL;
+                    scanf("Input message: %s",message.payload.to_all);
+                    send(socket_fd, &message, sizeof(message), MSG_DONTWAIT);
+                } else if (strncpy(request_type_input_buffer, "2ONE", 4) == 0) {
+                    message.request_type = TOONE;
+                    scanf("Input client: %s",message.payload.to_one.target_client);
+                    scanf("Input message: %s",message.payload.to_one.message);
+                    send(socket_fd, &message, sizeof(message), MSG_DONTWAIT);
+                } else {
+                    printf("Invalid request type!\n");
+                }
+
+                // ...
+                free(request_type_input_buffer);
+            } else
+                perror("scanf error");
+        }
+
+        request_message_t stop_message = {
+                .request_type = STOP
+        };
+
+        strncpy(stop_message.sender_client_id, client_identifier, MAX_CLIENT_ID_LEN);
+        send(socket_fd, &stop_message, sizeof(stop_message), MSG_DONTWAIT);
+    }
+
+    // ...
+    close(socket_fd);
+    return 0;
+}
